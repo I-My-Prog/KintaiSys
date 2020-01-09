cd8b7c5aac0eec25f848f0a9ba44e565a2518ce6
Akata_M <eitaroumo@gmail.com>
2020-01-09 13:54:13 +0900

    add: スキャンウィンドウ 登録ウインドウ スキャンウィンドウ・登録ウィンドウへの遷移

--- app/src/main/java/com/example/kintaisys/MainActivity.kt
+++ app/src/main/java/com/example/kintaisys/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.kintaisys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ViewAnimator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ボタンの取得
+        val btnTouroku = findViewById<Button>(R.id.Touroku)
+        val btnScan = findViewById<Button>(R.id.ScanButton)
+
+        //登録ボタンにリスナ登録
+        btnTouroku.setOnClickListener(object : View.OnClickListener {
+            override  fun onClick(v: View?){
+                //Intentクラスのオブジェクト生成
+                val intent = Intent(this@MainActivity, Touroku::class.java)
+                //生成オブジェクトを引数に画面を起動する
+                startActivity(intent)
+            }
+        })
+        //スキャンボタンにリスナ登録
+        btnScan.setOnClickListener(object : View.OnClickListener {
+            override  fun onClick(v: View?){
+                //Intentクラスのオブジェクト生成
+                val intent = Intent(this@MainActivity, Scan::class.java)
+                //生成オブジェクトを引数に画面を起動する
+                startActivity(intent)
+            }
+        })
     }
 }
